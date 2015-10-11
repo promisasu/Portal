6b3bdde8744f7a5e084cb21dfe4f0ff990388b41
Christian Murphy <Christian.Murphy@asu.edu>
2015-10-11 15:33:46 -0700

    dynamic trial content

--- presenter/trial.js
+++ presenter/trial.js
@@ -13,6 +13,43 @@
  */
 module.exports = function (request, reply) {
     reply.view('trial', {
-        title: 'Pain Reporting Portal'
+        title: 'Pain Reporting Portal',
+        trial: {
+            name: 'Sickle Cell',
+            description: 'A brief blurb describing the trial.',
+            start: '08/25/2015',
+            duration: '60 days',
+            patientCount: 1023,
+            noncompliantCount: 8
+        },
+        patients: [
+            {
+                id: 1234,
+                status: 'danger',
+                stage: 1,
+                surveyType: 'Daily',
+                lastTaken: '09/05/2015',
+                totalMissed: 5,
+                consecutiveMissed: 2
+            },
+            {
+                id: 6546,
+                status: 'success',
+                stage: 1,
+                surveyType: 'Weekly',
+                lastTaken: '09/07/2015',
+                totalMissed: 0,
+                consecutiveMissed: 0
+            },
+            {
+                id: 7865,
+                status: 'warning',
+                stage: 1,
+                surveyType: 'Daily',
+                lastTaken: '09/05/2015',
+                totalMissed: 1,
+                consecutiveMissed: 1
+            }
+        ]
     });
 };
