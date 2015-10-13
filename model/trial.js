23c5db11133f8e1499e23ce0357bf079088fccde
Christian Murphy <Christian.Murphy@asu.edu>
2015-10-12 17:33:14 -0700

    add trial properties

--- model/trial.js
+++ model/trial.js
@@ -22,7 +22,24 @@ module.exports = function (sequelize) {
     sequelize.define('trial',
         {
             name: {
-                type: Sequelize.STRING
+                type: Sequelize.STRING,
+                allowNull: false,
+                validate: {
+                    notEmpty: true,
+                    min: 3
+                }
+            },
+            description: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            startDate: {
+                type: Sequelize.DATE,
+                allowNull: false
+            },
+            endDate: {
+                type: Sequelize.DATE,
+                allowNull: false
             }
         },
         {
