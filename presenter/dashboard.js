fe504a7dbbc3266beac7ea9ab27112c87ff86a80
Becca Little <rebecca.little@asu.edu>
2015-10-25 14:43:42 -0700

    moved status column to dropdown button in upper right. consolidated patient category columns and added progress bars. removed duration column. changed date column names.

--- presenter/dashboard.js
+++ presenter/dashboard.js
@@ -86,16 +86,27 @@ function processTrial (currentTrial) {
     const endDate = moment(trial.endAt);
     const statuses = ['Pending', 'Upcoming', 'In Progress', 'Completed'];
     const status = statuses[Math.floor(Math.random() * 4)];
+    // TODO: Currently fake data, make this live data
+    const targetCount = Math.floor(Math.random() * 900 + 500);
+    const recruitedCount = targetCount - 153;
+    const activeCount = recruitedCount - 23;
+    const compliantCount = activeCount - 67;
 
     return {
         id: trial.id,
         name: trial.name,
         start: startDate.format('L'),
         end: endDate.format('L'),
-        duration: startDate.to(endDate, true),
-        // TODO: Currently fake data, make this live data
-        patientCount: Math.floor(Math.random() * 900 + 100),
-        noncompliantCount: Math.floor(Math.random() * 100),
+        targetCount: targetCount,
+        recruitedCount: recruitedCount,
+        activeCount: activeCount,
+        compliantCount: compliantCount,
+        recruitedPercent: ((recruitedCount / targetCount)*100).toFixed(2),
+        unrecruitedPercent: (100 - (recruitedCount / targetCount)*100).toFixed(2),
+        activePercent: ((activeCount / recruitedCount)*100).toFixed(2),
+        completedPercent: (100 - (activeCount / recruitedCount)*100).toFixed(2),
+        compliantPercent: ((compliantCount / activeCount)*100).toFixed(2),
+        noncompliantPercent: (100 - (compliantCount / activeCount)*100).toFixed(2),
         status: status
     };
 }
