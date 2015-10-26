92ecc079d861a847961d76e9b82f5d5f541cf7b8
Sylvia Barnai <sb_pink_girl@yahoo.com>
2015-10-25 20:32:45 -0700

    added patient categories to trial model

--- model/trial.js
+++ model/trial.js
@@ -48,7 +48,84 @@ module.exports = function (sequelize) {
             endAt: {
                 type: Sequelize.DATE,
                 allowNull: false
+            },
+            'use strict';
+
+/**
+ * @module model/trial
+ */
+
+const Sequelize = require('sequelize');
+
+/**
+ * a clinical Trial has many Patients taking Surveys
+ * @typedef {Object} Trial
+ * @property {String} name - trial name
+ */
+
+/**
+ * Registers model with Sequelize
+ * @function register
+ * @param {Sequelize} sequelize - database instance
+ * @returns {Null} nothing
+ */
+module.exports = function (sequelize) {
+    sequelize.define('trial',
+        {
+            name: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                validate: {
+                    notEmpty: true,
+                    min: 3
+                }
+            },
+            description: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            IRBID: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                validate: {
+                    notEmpty: true,
+                    min: 4
+                }
+            },
+            startAt: {
+                type: Sequelize.DATE,
+                allowNull: false
+            },
+            endAt: {
+                type: Sequelize.DATE,
+                allowNull: false
             }
+            dateStarted: {
+                type: Sequelize.DATE
+            },
+            dateCompleted: {
+                type: Sequelize.DATE
+            },
+            targetCount {
+                type: Sequelize.INTEGER
+            },
+            recruitedCount {
+                type: Sequelize.INTEGER
+            },
+            activeCount {
+                type: Sequelize.INTEGER
+            },
+            compliantCount {
+                type: Sequelize.INTEGER
+            }
+        },           
+        {
+            freezeTableName: true,
+            paranoid: true
+        }
+    );
+};
+
         },
         {
             freezeTableName: true,
