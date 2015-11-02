b9fdeb5e30960ca371168b2e1d3f918fa7da2dff
Pooja Rallabhandi <prallabh@asu.edu>
2015-11-01 19:44:02 -0700

    Modified files for adding new patient PIN

--- presenter/create-patient.js
+++ presenter/create-patient.js
@@ -25,10 +25,23 @@ module.exports = function (request, reply) {
     .then(function (data) {
         newPatient = data[0];
         const currentTrial = data[1];
-
+        const temp = addPadding(newPatient.id.toString(),newPatient.id.toString().length);
+        const pin = currentTrial.id.toString().concat(temp);
+        newPatient.pin = pin;
+        newPatient.save();
         return currentTrial.addPatient(newPatient);
     })
     .then(function () {
         reply.redirect('/patient/' + newPatient.id);
     });
 };
+
+function addPadding(pin, len){
+    const padding = "0";
+    while(len < 3){
+        console.log("Inside while loop");
+        pin = padding + pin;
+        len++;
+    }
+    return pin;
+}
