2044971a61a23e619a0d0daec21709c38e20ad62
Christian Murphy <Christian.Murphy@asu.edu>
2015-11-19 09:14:10 -0700

    setup basic auth

--- controller/server.js
+++ controller/server.js
@@ -4,25 +4,68 @@
 const path = require('path');
 const hapi = require('hapi');
 const vision = require('vision');
+const authBasic = require('hapi-auth-basic');
 const handlebars = require('handlebars');
 
 // load router and database
 const router = require('./router');
 const database = require('../model');
 
+/**
+ * Checks that a login is valid
+ * @param {Object} request - the Hapi request
+ * @param {String} username - username for login
+ * @param {String} password - password for login
+ * @param {Function} callback - alerts Hapi if login is valid or not
+ * @returns {Null} nothing
+ */
+function validate (request, username, password, callback) {
+    // TODO Use actual usernames and passwords, hash and salt passwords
+    const err = null;
+    const isValid = true;
+
+    return callback(
+        err,
+        isValid,
+        {
+            id: 1,
+            name: username
+        }
+    );
+}
+
 module.exports = function (configuration) {
     const server = new hapi.Server();
+
+    // configure server connection
+    server.connection({
+        port: configuration.server.port,
+        host: configuration.server.hostname
+    });
 
     // register hapi plugins
     server.register(
         [
             {
                 register: vision
+            },
+            {
+                register: authBasic
             }
         ],
         (err) => {
             if (err) {
                 console.log(err);
+            } else {
+                // Secure dashboard with login
+                server.auth.strategy(
+                    'simple',
+                    'basic',
+                    {
+                        validateFunc: validate
+                    }
+                );
+                server.auth.default('simple');
             }
         }
     );
@@ -43,12 +86,6 @@ module.exports = function (configuration) {
         helpersPath: 'helper',
         // sets default layout to 'layout/default.handlebars'
         layout: 'default'
-    });
-
-    // configure server connection
-    server.connection({
-        port: configuration.server.port,
-        host: configuration.server.hostname
     });
 
     // configure database connection
