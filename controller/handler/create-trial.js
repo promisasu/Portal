dc2d4e56d2fec8832d91b511dbfc62a1470f24f0
Kallol Chatterjee <kallol.chatterjee90@gmail.com>
2016-02-03 14:53:11 -0700

    Transaction Added

--- controller/handler/create-trial.js
+++ controller/handler/create-trial.js
@@ -17,34 +17,47 @@ function createTrial (request, reply) {
     const trial = database.sequelize.model('trial');
     const stage = database.sequelize.model('stage');
     let newTrial = null;
+    let transaction = null;
 
-    trial.create({
-        name: request.payload.name,
-        description: request.payload.description,
-        IRBID: request.payload.IRBID,
-        IRBStart: request.payload.IRBStart,
-        IRBEnd: request.payload.IRBEnd,
-        targetCount: request.payload.targetCount
+    database.sequelize.transaction()
+    .then((newTransaction) => {
+        transaction = newTransaction;
+        return trial.create({
+            name: request.payload.name,
+            description: request.payload.description,
+            IRBID: request.payload.IRBID,
+            IRBStart: request.payload.IRBStart,
+            IRBEnd: request.payload.IRBEnd,
+            targetCount: request.payload.targetCount
+        }, {transaction});
     })
     .then((nTrial) => {
+        console.error('next thing');
         newTrial = nTrial;
         const stagePromises = [];
+        const stageNames = request.payload.stageschedule.split(',');
 
-        for (let index = 0; index < request.payload.stagecount; index += 1) {
+        if (stageNames.length !== request.payload.stagecount) {
+            throw new Error('No of Stages not matched with Stage Schedule information given');
+        }
+        for (const stageName of stageNames) {
             stagePromises.push(
-                stage.create({name: request.payload.stageschedule})
+                stage.create({name: stageName}, {transaction})
             );
         }
-
         return Promise.all(stagePromises);
     })
     .then((newStages) => {
-        return newTrial.addStages(newStages);
+        return newTrial.addStages(newStages, {transaction});
+    })
+    .then(() => {
+        return transaction.commit();
     })
     .then(() => {
         reply.redirect(`/trial/${newTrial.id}`);
     })
     .catch((err) => {
+        transaction.rollback();
         console.error(err);
         reply(boom.badRequest('Invalid Trial'));
     });
