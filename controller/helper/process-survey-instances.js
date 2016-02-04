b19e39421fe95b04ce4e886e80b819c66b1df0cc
Snehal Shendware <snehalshendware1@gmail.com>
2016-02-04 11:00:51 -0700

    Fix of some lint error for US #377
    Yet to resolve full list of lint problems.

--- controller/helper/process-survey-instances.js
+++ controller/helper/process-survey-instances.js
@@ -5,54 +5,69 @@
  */
 
 /**
- * Takes in a Survey Instances and processes it to get complence chart details
- * @param {Surveys} list of survey instances
- * @returns {Object}  Complience chart data
+ * Takes in a Survey Instances and processes it to get Complience chart details
+ * @param {Object} surveys - list of survey instances
+ * @returns {Object} Complience chart data
  */
 function processSurveyInstances (surveys) {
     return {
-      labels:pickupSubmissionTime(surveys),
-      datasets:calculateTimeLeft(surveys)
+        labels: pickupSubmissionTime(surveys),
+        datasets: calculateTimeLeft(surveys)
     };
 }
 
-function pickupSubmissionTime(surveys) {
-  var dates = [];
-  dates.length = surveys.length;
-  for (var i = 0; i < surveys.length; i++) {
-    dates[i] = formatDate(surveys[i].startTime);
-  }
-  return dates;
+/**
+ * Takes in a Survey Instances and get the dates to be shown on Complience chart
+ * @param {Object} surveys - list of survey instances
+ * @returns {Object} list of dates labels
+ */
+function pickupSubmissionTime (surveys) {
+    const dates = [];
+
+    for (let index = 0; index < surveys.length; index += 1) {
+      dates[index] = formatDate(surveys[index].startTime);
+    }
+    return dates;
 }
 
-function formatDate(date) {
-  var hours = date.getHours();
-  var minutes = date.getMinutes();
-  minutes = minutes < 10 ? '0'+minutes : minutes;
-  var strTime = hours + ':' + minutes;
-  return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime;
+/**
+ * Takes in a date as per DB format and process it to complience chart format
+ * @param {Object} date - DB formatted Date
+ * @returns {Object} formatted date
+ */
+function formatDate (date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    let strTime = hours + ':' + minutes;
+    return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime;
 }
 
-function calculateTimeLeft(surveys) {
-  var percentages = [];
+/**
+ * Takes in a Survey Instances and get the % time left to be shown on complience chart
+ * @param {Object} surveys - list of survey instances
+ * @returns {Object} processed list of % time left data
+ */
+function calculateTimeLeft (surveys) {
+    const percentages = [];
 
-  for (var i = 0; i < surveys.length; i++) {
-    var startTimeStamp = (Date.parse(surveys[i].startTime))/3600000;
-    var endTimeStamp = (Date.parse(surveys[i].endTime))/3600000;
-    var actualTimeStamp = (Date.parse(surveys[i].actualSubmissionTime))/3600000;
-    var timeToCompleteSurvey = endTimeStamp - startTimeStamp;
-    var timeTaken = endTimeStamp - actualTimeStamp;
-    if (timeTaken > 0) {
-      percentages[i] = Math.round((timeTaken/timeToCompleteSurvey)*100);
+    for (let index = 0; index < surveys.length; index += 1) {
+      let startTimeStamp = (Date.parse(surveys[index].startTime))/3600000;
+      let endTimeStamp = (Date.parse(surveys[index].endTime))/3600000;
+      let actualTimeStamp = (Date.parse(surveys[index].actualSubmissionTime))/3600000;
+      let timeToCompleteSurvey = endTimeStamp - startTimeStamp;
+      let timeTaken = endTimeStamp - actualTimeStamp;
+      if (timeTaken > 0) {
+        percentages[index] = Math.round((timeTaken/timeToCompleteSurvey)*100);
+      }
+      else {
+        percentages[index] = 0;
+      }
     }
-    else {
-      percentages[i] = 0;
-    }
-  }
 
-  var calData = [{label:'% Time left until daily survey expired',
-                  data: percentages}];
-  return calData;
+    const calData = [{label:'% Time left until daily survey expired',
+                    data: percentages}];
+    return calData;
 
 }
 module.exports = processSurveyInstances;
