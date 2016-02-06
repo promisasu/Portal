8a41420bc03f441ec6e93ee4e1c98c42ea777176
Snehal Shendware <snehalshendware1@gmail.com>
2016-02-06 13:28:26 -0700

    Per week view changes for compliance Chart
    1. Per week change of calendar view, compliance chart dates should vary.

--- view/script/patient.js
+++ view/script/patient.js
@@ -3,7 +3,7 @@
 
     var config = {
         type: 'line',
-        data: window.dates,
+        data: '',
         options: {
             scales: {
                 xAxes: [
@@ -38,7 +38,9 @@
         }
     };
 
+    var allDatesConfig = jQuery.extend(true, {}, window.dates);
     var ctx = document.getElementById('complianceChart').getContext('2d');
+    var perWeekDatesConfig = [];
 
     $('#calendar').fullCalendar({
         defaultView: 'basicWeek',
@@ -46,6 +48,48 @@
         contentHeight: 'auto',
         events: window.events
     });
+
+    perWeekDatesConfig = datesByWeek(allDatesConfig);
+    config.data = perWeekDatesConfig;
+
+    $('.fc-prev-button').click(function() {
+        perWeekDatesConfig = datesByWeek(allDatesConfig);
+        config.data = perWeekDatesConfig;
+        new Chart(ctx, config);
+    });
+
+    $('.fc-next-button').click(function() {
+        perWeekDatesConfig = datesByWeek(allDatesConfig);
+        config.data = perWeekDatesConfig;
+        new Chart(ctx, config);
+    });
+
+    function datesByWeek(configData) {
+
+        var datesConfig = jQuery.extend(true, {}, configData);
+        var view = $('#calendar').fullCalendar('getView');
+        var datesPerView = [];
+        var timeLeftData = [];
+
+        var weekStartDate = new Date(view.intervalStart).getTime();
+        var weekEndDate = new Date(view.intervalEnd).getTime();
+        var index = 0;
+        var dateLabel = '';
+        var dateDatasetData = '';
+
+        for (index = 0; index < datesConfig.labels.length; index += 1) {
+            dateLabel = new Date(datesConfig.labels[index]).getTime();
+            dateDatasetData = datesConfig.datasets[0].data[index];
+            if (dateLabel >= weekStartDate && dateLabel <= weekEndDate) {
+                datesPerView.push(dateLabel);
+                timeLeftData.push(dateDatasetData);
+            }
+        }
+
+        datesConfig.labels = datesPerView;
+        datesConfig.datasets[0].data = timeLeftData;
+        return datesConfig;
+    }
 
     new Chart(ctx, config);
 }());
