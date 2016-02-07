1e1bca847152ae3912666b889cbfe480ba44e865
mpenumal <mpenumal@asu.edu>
2016-02-07 09:31:00 -0700

    Code review comments implemented.

--- controller/helper/process-survey-instances.js
+++ controller/helper/process-survey-instances.js
@@ -11,24 +11,12 @@ const moment = require('moment');
  */
 function processSurveyInstances (surveys) {
     return {
-        labels: pickupSubmissionTime(surveys),
+        // Using UTC as the date gets modified to the local time (GMT in this case) when UTC not used.
+        labels: surveys.map((survey) => {
+            return moment(survey.startTime).utc().format('MM/DD/YYYY HH:mm');
+        }),
         datasets: pickTimeLeft(surveys)
     };
-}
-
-/**
- * Takes in a Survey Instances and get the dates to be shown on Complience chart
- * @param {Object} surveys - list of survey instances
- * @returns {Object} list of dates labels
- */
-function pickupSubmissionTime (surveys) {
-    const dates = [];
-
-    for (let index = 0; index < surveys.length; index += 1) {
-        // dates[index] = formatDate(surveys[index].startTime);
-        dates[index] = moment(surveys[index].startTime).utc().format('MM/DD/YYYY HH:mm');
-    }
-    return dates;
 }
 
 /**
@@ -37,26 +25,26 @@ function pickupSubmissionTime (surveys) {
  * @returns {Object} processed list of % time left data
  */
 function pickTimeLeft (surveys) {
-    const percentages = [];
+    // Using UTC as the date gets modified to the local time (GMT in this case) when UTC not used.
+    const percentages = surveys.map((suvey) => {
+        return calculateTimeLeft(moment(suvey.startTime).utc(),
+                                moment(suvey.endTime).utc(),
+                                moment(suvey.actualSubmissionTime).utc());
+    });
 
-    for (let index = 0; index < surveys.length; index += 1) {
-        percentages[index] = calculateTimeLeft(moment(surveys[index].startTime).utc(),
-                                                moment(surveys[index].endTime).utc(),
-                                                moment(surveys[index].actualSubmissionTime).utc());
-    }
-    const calData = [{label: '% Time left until daily survey expired',
-                    data: percentages}];
-
-    return calData;
+    return [{
+        label: '% Time left until daily survey expired',
+        data: percentages
+    }];
 }
 
 /**
-* Takes in a Survey Instances and get the % time left to be shown on complience chart
-* @param {Object} startTime - start time of survey instance
-* @param {Object} endTime - end time of survey instance
-* @param {Object} actualTime - actual time of survey instance
-* @returns {Object} percent time left after completing survey instance
-*/
+ * Takes in a Survey Instances and get the % time left to be shown on complience chart
+ * @param {Object} startTime - start time of survey instance
+ * @param {Object} endTime - end time of survey instance
+ * @param {Object} actualTime - actual time of survey instance
+ * @returns {Object} percent time left after completing survey instance
+ */
 function calculateTimeLeft (startTime, endTime, actualTime) {
     let percentTimeLeft = 0;
     const percent = 100;
