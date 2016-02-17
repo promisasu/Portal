47a22de929e73ad3d520ad464a55d2f045735a11
Christian Murphy <Christian.Murphy@asu.edu>
2016-02-17 09:49:49 -0700

    gathers all patient data together
    update stylelint

--- controller/handler/patient-csv.js
+++ controller/handler/patient-csv.js
@@ -4,7 +4,8 @@
  * @module controller/handler/patient-csv
  */
 
-// const database = require('../../model');
+const database = require('../../model');
+const boom = require('boom');
 
 /**
  * Create a Comma Seperate Value export of a single patient's data.
@@ -13,8 +14,61 @@
  * @returns {View} Rendered page
  */
 function patientCSV (request, reply) {
-    // TODO get and transform data
-    reply({message: 'success'});
+    Promise
+    .all([
+        // All of the responses for the patient's surveys
+        database.sequelize.query(
+            `
+            SELECT si.id AS surveyId, qo.id AS optionId
+            FROM patient AS pa
+            JOIN survey_instance AS si
+            ON si.patientId = pa.id
+            JOIN question_result AS qr
+            ON qr.surveyInstanceId = si.id
+            JOIN question_option AS qo
+            ON qo.id = qr.questionOptionId
+            WHERE pa.pin = ?
+            `,
+            {
+                type: database.sequelize.QueryTypes.SELECT,
+                replacements: [
+                    request.params.id
+                ]
+            }
+        ),
+        // All patient data, survey data and question data
+        // both answered and unanswered
+        database.sequelize.query(
+            `
+            SELECT *, si.id AS surveyId, qt.id AS questionId, qo.id AS optionId
+            FROM patient AS pa
+            JOIN survey_instance AS si
+            ON si.patientId = pa.id
+            JOIN survey_template AS st
+            ON st.id = si.surveyTemplateId
+            JOIN join_surveys_and_questions AS jsq
+            ON jsq.surveyTemplateId = st.id
+            JOIN question_template AS qt
+            ON qt.id = jsq.questionTemplateId
+            JOIN question_option AS qo
+            ON qo.questionTemplateId = qt.id
+            WHERE pa.pin = ?
+            `,
+            {
+                type: database.sequelize.QueryTypes.SELECT,
+                replacements: [
+                    request.params.pin
+                ]
+            }
+        )
+    ])
+    .then((data) => {
+        reply(data);
+    })
+    .catch((err) => {
+        console.error(err);
+        reply(boom.notFound('patient data not found'));
+    });
 }
 
 module.exports = patientCSV;
