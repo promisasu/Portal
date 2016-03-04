d9eb91cb4bf34448d52249c3c063aa45d8f914dc
Christian Murphy <Christian.Murphy@asu.edu>
2016-03-03 20:13:45 -0700

    update 'npm run stop' command

---
+++ task/stop-scheduler.js
@@ -0,0 +1,15 @@
+'use strict';
+const processManager = require('pm2');
+const configuration = require('../config.json');
+
+processManager.connect(() => {
+    processManager.delete(
+        `prp-${configuration.environment}-scheduler`,
+        (err) => {
+            if (err) {
+                console.error(err);
+            }
+            processManager.disconnect();
+        }
+    );
+});
