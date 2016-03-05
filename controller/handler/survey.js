8751cb5b1cf2af8e0f6f0fdb2463ceaf66143198
Christian Murphy <Christian.Murphy@asu.edu>
2016-03-05 16:18:09 -0700

    use left join to gather info for survey view

--- controller/handler/survey.js
+++ controller/handler/survey.js
@@ -16,26 +16,6 @@ const database = require('../../model');
 function surveyView (request, reply) {
     Promise
     .all([
-        // all of the responses for the survey
-        database.sequelize.query(
-            `
-            SELECT qo.id
-            FROM survey_instance AS si
-            JOIN question_result AS qr
-            ON qr.surveyInstanceId = si.id
-            JOIN question_option AS qo
-            ON qo.id = qr.questionOptionId
-            WHERE si.id = ?
-            `,
-            {
-                type: database.sequelize.QueryTypes.SELECT,
-                replacements: [
-                    request.params.id
-                ]
-            }
-        ),
-        // all the questions and optional
-        // includes both answered and unanswered
         database.sequelize.query(
             `
             SELECT *, si.id AS surveyId, qt.id AS questionId, qo.id AS optionId
@@ -48,6 +28,9 @@ function surveyView (request, reply) {
             ON qt.id = jsq.questionTemplateId
             JOIN question_option AS qo
             ON qo.questionTemplateId = qt.id
+            LEFT JOIN question_result AS qr
+            ON qr.surveyInstanceId = si.id
+            AND qr.questionOptionId = qo.id
             WHERE si.id = ?
             ORDER BY jsq.questionOrder, qo.order
             `,
@@ -80,24 +63,8 @@ function surveyView (request, reply) {
         )
     ])
     .then((data) => {
-        const surveyResponses = data[0].map((item) => {
-            return item.id;
-        });
-        const surveyInstanceAndQuestions = data[1];
-        const patientAndTrial = data[2];
-        const negative = -1;
-
-        let questionsWithResponses = surveyInstanceAndQuestions.map((item) => {
-            if (surveyResponses.indexOf(item.optionId) > negative) {
-                item.selected = true;
-
-                return item;
-            }
-
-            return item;
-        });
-
-        questionsWithResponses = groupBy(questionsWithResponses, 'questionId');
+        const questionsWithResponses = groupBy(data[0], 'questionId');
+        const patientAndTrial = data[1];
 
         reply.view('survey', {
             title: 'Pain Reporting Portal',
@@ -108,7 +75,7 @@ function surveyView (request, reply) {
                 id: patientAndTrial.id,
                 name: patientAndTrial.name
             },
-            survey: surveyInstanceAndQuestions[0],
+            survey: questionsWithResponses[0],
             questions: questionsWithResponses
         });
     })
