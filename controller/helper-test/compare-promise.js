6cd13704a71fe4a44b9c09656765105d3181a0ca
Christian Murphy <Christian.Murphy@asu.edu>
2016-04-04 17:44:29 -0700

    check for Promise related code smells

--- controller/helper-test/compare-promise.js
+++ controller/helper-test/compare-promise.js
@@ -10,6 +10,10 @@ test.cb('when values are the same', (t) => {
     .then((isValid) => {
         t.true(isValid, 'it should be valid');
         t.end();
+    })
+    .catch(() => {
+        t.fail();
+        t.end();
     });
 });
 
@@ -17,6 +21,10 @@ test.cb('when values are different', (t) => {
     comparePromise('test', 'not')
     .then((isValid) => {
         t.false(isValid, 'it should not be valid');
+        t.end();
+    })
+    .catch(() => {
+        t.fail();
         t.end();
     });
 });
