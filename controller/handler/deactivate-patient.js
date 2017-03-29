d47a2a543408e20dfffbe31f5756ba69bb1d5b00
Pani Aravind K V <Aravind@panis-mbp.mobile.asu.edu>
2017-03-29 15:09:20 -0700

    deactivate patients removed comments

--- controller/handler/deactivate-patient.js
+++ controller/handler/deactivate-patient.js
@@ -15,37 +15,6 @@ const moment = require('moment');
  * @returns {Null} Redirect
  */
 function deactivatePatient(request, reply) {
-    // const patient = database.sequelize.model('patient');
-    // let transaction = null;
-
-    // database
-    // .sequelize
-    // .transaction()
-    // .then((newTransaction) => {
-    //     transaction = newTransaction;
-
-    //     return patient.findOne({
-    //         where: {
-    //             pin: request.params.pin
-    //         },
-    //         transaction
-    //     });
-    // })
-    // .then((currentPatient) => {
-    //     return currentPatient.destroy({transaction});
-    // })
-    // .then(() => {
-    //     return transaction.commit();
-    // })
-    // .then(() => {
-    //     return reply();
-    // })
-    // .catch((err) => {
-    //     request.log('error', err);
-    //     transaction.rollback();
-
-    //     return reply(boom.conflict());
-    // });
 
     database.sequelize.query(
         `
