4d70f5c559f7edab9cc080ea4188d69ff333b2ed
Mohit Mulchandani <Mohit.Mulchandani@asu.edu>
2017-04-18 15:58:11 -0700

    Minor fixes required in semi-compliance computation

--- controller/helper/process-compliance-count.js
+++ controller/helper/process-compliance-count.js
@@ -9,11 +9,13 @@
  * @returns {Array<Number>} number of non-compliant, semi-compliant and compliant patients
  */
 function processComplianceCount (rows) {
-    console.log('Outputting rows from processComplianceCount -----------------------');
-    console.log(rows);
-    const nonCompliantThreshold = 4;
+    for (const row of rows){
+      row.expiredWeeklyCount = row.expiredWeeklyCount * 3;
+      row.completedWeeklyCount = row.completedWeeklyCount * 3;
+    }
+    const nonCompliantThreshold = 66.67;
     const increment = 1;
-    const semiCompliantThreshold = 0;
+    const semiCompliantThreshold = 33.33;
     const compliance = {
         compliant: 0,
         semiCompliant: 0,
@@ -21,10 +23,11 @@ function processComplianceCount (rows) {
     };
 
     for (const row of rows) {
-        if (row.expiredCount > nonCompliantThreshold) {
+        var compliancePercentage = (row.expiredWeeklyCount + row.expiredDailyCount)/(row.expiredWeeklyCount + row.expiredDailyCount + row.completedWeeklyCount + row.completedDailyCount);
+        if (compliancePercentage >= nonCompliantThreshold) {
             compliance.nonCompliant += increment;
-        // } else if (row.expiredCount > semiCompliantThreshold) {
-        //     compliance.semiCompliant += increment;
+        } else if (compliancePercentage >= semiCompliantThreshold) {
+            compliance.semiCompliant += increment;
         } else {
             compliance.compliant += increment;
         }
